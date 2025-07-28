a737fcd3ad90119a68d4508ac0e10a52a042cd1c
networkSocerer <gigattfyhig@gmail.com>
2025-07-28 18:50:08 +0900

    login && sighup

--- todo-be/controller/user.controller.js
+++ todo-be/controller/user.controller.js
@@ -35,7 +35,9 @@ userController.loginWithEmail = async (req, res) => {
       }
     }
     throw new Error("아이디 또는 비밀번호가 일치 하지 않습니다.");
-  } catch (error) {}
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
 };
 
 module.exports = userController;
